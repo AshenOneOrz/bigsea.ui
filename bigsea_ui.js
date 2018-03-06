c834cb9def8b6bc411e3306fa287b01d0f832b49
bigsea <iwangyang@qq.com>
2018-03-06 17:22:52 +0800

    分页

--- bigsea_ui.js
+++ bigsea_ui.js
@@ -1,4 +1,4 @@
-// HTML
+// Html 弹窗
 Sea.innerHTML = `<sea class='confirm'>
     <div class="cont">
         <text class="msg"></text>
@@ -9,39 +9,65 @@ Sea.innerHTML = `<sea class='confirm'>
     </div>
     </sea>`.html()
 Sea('body').append(Sea.innerHTML)
+// Html 分页
+Sea.innerHTML = `<sea class="input-group">
+        <sea class="btn">
+            <i class="iconfont icon-left"></i>
+        </sea>
+        <input value="1" maxlength="3" placeholder="255">
+        <sea class="btn">
+            <i class="iconfont icon-right"></i>
+        </sea>
+        <sea class="btn primary">
+            <text>跳页</text>
+        </sea>
+    </sea>`.html()
+Sea('sea.pagination').html(Sea.innerHTML)
 
-// Event confirm
-let confirm = Sea('sea.confirm')
-let callback = function(bool) {
-    e.hide()
-    let f = Sea.confirm.callback
-    if (f && typeof f == 'function') {
-        f(bool)
-        Sea.confirm.callback = undefined
-    } else {
-        // log("not callback")
-    }
+// Events
+Sea.bindEvent = {
+    btn() {
+        Sea('sea.btn').on('mousedown', function(event) {
+            let e = Sea(this)
+            e.addClass('clicked')
+            setTimeout(function() {
+                e.removeClass('clicked')
+            }, 400)
+            Sea.confirm('测试')
+        })
+        Sea('sea.btn-group').on('mousedown', '.btn', function(event, index) {
+            let element = Sea(this)
+            let group = element.parent()
+            group.data('now', index)
+            group.find('.btn').removeClass('checked')
+            element.addClass('checked')
+        })
+    },
+    confirm() {
+        let confirm = Sea('sea.confirm')
+        let callback = function(bool) {
+            confirm.hide()
+            let f = Sea.confirm.callback
+            if (f && typeof f == 'function') {
+                f(bool)
+                Sea.confirm.callback = undefined
+            } else {
+                // log("not callback")
+            }
+        }
+        confirm.find('.ok').on('click', function() {
+            callback(true)
+        })
+        confirm.find('.no').on('click', function() {
+            callback(false)
+        })
+    },
+    pagination() {
+
+    },
 }
-confirm.find('.ok').on('click', function() {
-    callback(true)
-})
-confirm.find('.no').on('click', function() {
-    callback(false)
-})
 
-// Event btn
-Sea('sea.btn').on('mousedown', function() {
-    let e = Sea(this)
-    e.addClass('clicked')
-    setTimeout(function() {
-        e.removeClass('clicked')
-    }, 400)
-    // Sea.confirm('测试')
-})
-Sea('sea.btn-group').on('mousedown', '.btn', function(event, index) {
-    let element = Sea(this)
-    let group = element.parent()
-    group.data('now', index)
-    group.find('.btn').removeClass('checked')
-    element.addClass('checked')
+// 执行
+Object.keys(Sea.bindEvent).forEach(key => {
+    Sea.bindEvent[key]()
 })
